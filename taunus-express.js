61691ae19215f1fd2e13bc0956995b6b272e8a5b
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-11-09 00:46:19 -0300

    simplified middleware

--- taunus-express.js
+++ taunus-express.js
@@ -3,27 +3,23 @@
 var pkg = require('./package.json');
 
 function factory (taunus, app, options) {
-  var render = taunus.render;
-
   taunus.mount(addRoute, options);
 
   function addRoute (d) {
-    app.get.apply(app, parseMiddleware(d));
+    app.get.apply(app, flatten(d));
   }
 
-  function parseMiddleware (d) {
+  function flatten (d) {
     var flat = [d.route].concat(d.middleware);
     if (d.actionFn) {
       flat.push(d.actionFn);
     }
-    flat.push(renderFactory(d.action));
+    flat.push(render);
     return flat;
-  }
 
-  function renderFactory (defaultAction) {
-    return function renderMiddleware (req, res, next) {
-      render(defaultAction, res.viewModel, req, res, next);
-    };
+    function render (req, res, next) {
+      taunus.render(d.action, res.viewModel, req, res, next);
+    }
   }
 }
 
