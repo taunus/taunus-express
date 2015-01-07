63a15e149f6e297e87472849de8421fcc0b5bfb7
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-01-07 13:09:19 -0300

    res.redirect improvements

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 2.0.0 Convenience Store
+
+- `res.redirect` translates into `taunus.redirect` on Taunus view routes
--- taunus-express.js
+++ taunus-express.js
@@ -1,6 +1,9 @@
 'use strict';
 
 function factory (taunus, app, options) {
+  var _render = taunus.render.bind(taunus);
+
+  taunus.render = taunusRender;
   taunus.mount(addRoute, options);
 
   function addRoute (d) {
@@ -8,16 +11,41 @@ function factory (taunus, app, options) {
   }
 
   function flatten (d) {
-    var flat = [d.route].concat(d.middleware);
+    var flat = [d.route, mangle].concat(d.middleware);
     if (d.actionFn) {
       flat.push(d.actionFn);
     }
-    flat.push(render);
+    flat.push(renderResponse);
     return flat;
 
-    function render (req, res, next) {
+    function renderResponse (req, res, next) {
       taunus.render(d, res.viewModel, req, res, next);
     }
+  }
+
+  function mangle (req, res, next) {
+    res.__redirect = res.redirect;
+    res.redirect = redirect;
+    next();
+
+    function redirect (status, url) {
+      if (arguments.length === 1) {
+        url = status;
+      }
+      taunus.redirect(req, res, url);
+    }
+  }
+
+  function unmangle (res) {
+    if (res && res.__redirect) {
+      res.redirect = res.__redirect;
+      res.__redirect = void 0;
+    }
+  }
+
+  function taunusRender (route, vm, req, res, next) {
+    unmangle(res);
+    return _render.apply(null, arguments);
   }
 }
 
